20b33115215cb0ce8be4dc446e41e06263a9593a
NureBondarMykyta <167449741+NureBondarMykyta@users.noreply.github.com>
2024-09-19 13:24:29 +0300

    Initial Commit

---
+++ MemorizeApp/MemorizeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeAppApp.swift
+//  MemorizeApp
+//
+//  Created by Никита Бондарь on 19.09.2024.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
